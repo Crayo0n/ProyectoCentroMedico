9c04ee840b1234372de6deb1bd9c9266caa758fa
Crayo0n <mauricio28rd@gmail.com>
2025-06-22 20:01:36 -0600

    Interfaz para Descargar la Receta

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,37 +11,46 @@ def login():
 #Modulo de Médicos
 @app.route('/medicos')
 def doctores():
-    return render_template('/Medicos/medicos.html')
+    return render_template('Medicos/medicos.html')
 
 @app.route('/medicos/agregar')
 def doctores_agregar():
-    return render_template('/Medicos/agregar_medico.html')
+    return render_template('Medicos/agregar_medico.html')
 
 @app.route('/medicos/editar')
 def doctores_editar():
-    return render_template('/Medicos/editar_medico.html')
+    return render_template('Medicos/editar_medico.html')
 
 
 #Modulo de Pacientes
 @app.route('/pacientes')
 def pacientes():
-    return render_template('/Pacientes/pacientes.html')
+    return render_template('Pacientes/pacientes.html')
 
 @app.route('/pacientes/agregar')
 def pacientes_agregar():
-    return render_template('/Pacientes/agregar_pacientes.html')
+    return render_template('Pacientes/agregar_pacientes.html')
 
 @app.route('/pacientes/editar')
 def pacientes_editar():
-    return render_template('/Pacientes/editar_pacientes.html')
+    return render_template('Pacientes/editar_pacientes.html')
 
 @app.route('/pacientes/exploracion')
 def paciente_exploracion():
-    return render_template('exploracion_paciente.html')
+    return render_template('Pacientes/exploracion_paciente.html')
 
 @app.route('/pacientes/exploracion/editar')
 def exploracion_editar():
-    return render_template('exploracion_editar.html')
+    return render_template('Pacientes/exploracion_editar.html')
+
+@app.route('/pacientes/citas')
+def pacientes_citas():
+    return render_template('Pacientes/citas_paciente.html')
+
+@app.route('/pacientes/citas/diagnostico')
+def pacientes_diagnostico():
+    return render_template('Pacientes/diagnostico_paciente.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
